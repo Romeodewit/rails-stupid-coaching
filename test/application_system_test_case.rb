099a3cd47f1885f0f8ca583cd26bc208b72bde6a
Romeo de Wit <romeo@classicremedies.co.uk>
2020-11-09 15:36:53 +0000

    done

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.register_driver :headless_chrome do |app|
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'goog:chromeOptions': { args: %w(headless window-size=1280,1024 no-sandbox enable-features=NetworkService,NetworkServiceInProcess  disable-features=VizDisplayCompositor) } )
+        Watir::Browser.new(:chrome,
+                      :prefs => {:password_manager_enable => false, :credentials_enable_service => false},
+                      :switches => ["disable-infobars", "no-sandbox"])
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+  end
+  driven_by :selenium, using: :headless_chrome
 end
